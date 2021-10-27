24c567db54dbf47085ebdec202da0783d378b280
Akhilakhi96 <93140497+Akhilakhi96@users.noreply.github.com>
2021-10-27 23:01:12 +0530

    comitted user authenication module

---
+++ Task 5_user authenication.py
@@ -0,0 +1,37 @@
+#user authenication
+
+name=input("enter username: ")
+password=input("enter password: ")
+user_name="Akhil"
+psw="Akhil@akhi1"
+if name==(user_name):
+    if psw==(password):
+        print('you are logined succesfull')
+    else:
+        print('the enterterd password is incorrect')  
+else:
+    print('the enterterd username is incorrect')
+
+
+#-------------------------------------------------------------------------------------------------------------
+
+#user authenication
+
+name=input("enter username: ")
+password=input("enter password: ")
+user_name="Akhil"
+psw="Akhil@akhi1"
+if name==user_name and psw==password:
+    print('you are loggined succesfull')
+else:
+    if name!=user_name and psw!=password:
+        print('The entered password and username are not correct')
+    elif name!=user_name:
+        print('the enterterd username is incorrect')
+    else:
+        print('the enterterd password is incorrect')
+
+
+
+
+
